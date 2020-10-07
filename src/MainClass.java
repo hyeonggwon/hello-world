964f1c306203be2325680270d0bc3b7e6468a922
Kim <Kim@DESKTOP-UDSTMHM>
2020-10-07 15:07:42 +0900

    initial commit

---
+++ src/MainClass.java
@@ -0,0 +1,15 @@
+
+public class MainClass {
+	
+	public static void main(String[] args) {
+		
+		System.out.println(Math.sqrt(2));
+		
+		MethodClass methodClass = new MethodClass();
+		
+		System.out.println("1~100의 합 : " + methodClass.total());
+		
+		System.out.println("1~100의 합 : " + MethodClass.total());
+	}
+	
+}
